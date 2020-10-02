304bc4c29dbfd6b7d2ddff43f3474160027cf06f
Marshalla Robinson <marshallarbnsn@gmail.com>
2020-10-02 17:39:05 -0500

    added temp

--- project/js/main.js
+++ project/js/main.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
 })
 
 function getWeather(){
-  var url ="https://api.openweathermap.org/data/2.5/weather?q=Temple&APPID="+apiKey;
+  var url ="https://api.openweathermap.org/data/2.5/weather?q=Temple&units=imperial&APPID="+apiKey;
   
   $.ajax(url,{success: function(data){
-    console.log(data);
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
 
   }})
 }
