b5f53fefb93b35e93d8519a2ba02b64688d04b8b
Marshalla Robinson <marshallarbnsn@gmail.com>
2020-10-02 16:16:32 -0500

    Fixed error

--- project/js/main.js
+++ project/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 })
 
 function getWeather(){
-  var url =  "https://api.openweathermap.org / data / 2.5 / weather ? q = Temple & appid" + apikey;
+  var url =  "https://api.openweathermap.org / data / 2.5 /weather?q=Temple&APPID="+apikey;
   $.ajax(url,{success: function(data){
     console.log(data);
   }})
