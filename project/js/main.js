735186b0507f84edb92528e363b449281254a282
Marshalla Robinson <marshallarbnsn@gmail.com>
2020-10-02 19:47:26 -0500

    add

--- project/js/main.js
+++ project/js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){ 
+//$(document).ready(function(){ 
   //getWeather();
-})
+//})
 
 function getWeather(searchQuery){
   var url ="https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&APPID="+apiKey;
@@ -9,6 +9,8 @@ function getWeather(searchQuery){
     $(".city").text(data.name);
     $(".temp").text(data.main.temp);
 
+  },error: function(error){
+    $(".error-message").text("An error occured");
   }})
 }
 
